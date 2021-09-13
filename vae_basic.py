451dbb19c76a70b30e03fa2cfced22238ffa67d3
adsampat adsampat <adsampat@dsmlp-login.ucsd.edu>
2021-09-13 08:07:18 -0700

    VAE Updates

--- vae_basic.py
+++ vae_basic.py
@@ -7,9 +7,13 @@ import datetime
 import numpy as np
 import math
 import time
+from keras import backend as K
 
-start = time.time()
-with open('binned_gnps.pkl', 'rb') as f:
+epochs = 25000
+tag= "GNPS_Filtered"
+
+'''
+with open('binned_filtered_0.5.pkl', 'rb') as f:
     data = pickle.load(f)
     
 spectra_matrix = data[0].toarray().T
@@ -22,28 +26,49 @@ x_train = np.delete(x_train, test_index, 0)
 print(x_train.shape)
 print(x_test.shape)
 
+pickle.dump((x_train,x_test, test_index),open("filtered_data.pkl", "wb"))
+'''
+
+with open("filtered_data.pkl", 'rb') as f:
+    x_train, x_test, indices = pickle.load(f)
+
 dims = x_train.shape
 
-# This is our input data
-input_data = keras.Input(shape=(dims[1],))
-encoded = layers.Dense(100, activation='relu')(input_data)
+original_dim = dims[1]
+intermediate_dim = 100
+latent_dim = 20
 
-decoded = layers.Dense(dims[1], activation='sigmoid')(encoded) 
+inputs = keras.Input(shape=(original_dim,))
+h = layers.Dense(intermediate_dim, activation='relu')(inputs)
+z_mean = layers.Dense(latent_dim)(h)
+z_log_sigma = layers.Dense(latent_dim)(h)
+
+def sampling(args):
+    z_mean, z_log_sigma = args
+    epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dim),
+                              mean=0., stddev=0.1)
+    return z_mean + K.exp(z_log_sigma) * epsilon
+
+z = layers.Lambda(sampling)([z_mean, z_log_sigma])
+
+# This is our input data
+
+encoder = keras.Model(inputs, [z_mean, z_log_sigma, z], name='encoder')
+
+# Create decoder
+latent_inputs = keras.Input(shape=(latent_dim,), name='z_sampling')
+x = layers.Dense(intermediate_dim, activation='relu')(latent_inputs)
+outputs = layers.Dense(original_dim, activation='sigmoid')(x)
+decoder = keras.Model(latent_inputs, outputs, name='decoder')
 
 # This model maps an input to its reconstruction
-autoencoder = keras.Model(input_data, decoded)
+outputs = decoder(encoder(inputs)[2])
+autoencoder = keras.Model(inputs, outputs)
 
 autoencoder.compile(optimizer='adam', loss='mean_squared_error', metrics=[tfa.metrics.r_square.RSquare(dtype=tf.float32, y_shape=(dims[1],))])
 
-log_dir = "logs/fit/BasicVAE" + "_" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+log_dir = "logs/fit/"+ tag + "_BasicVAE_" + str(epochs) + "_" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-fit = time.time()
-autoencoder.fit(x_train, x_train, epochs=7000, callbacks=[tensorboard_callback], validation_data=(x_test, x_test))
-
-end = time.time()
-with open("times", 'a') as file:
-    file.write("Basic\n")
-    file.write("Total: " + str(end - start) + "\n")
-    file.write("Model Fit:" + str(end - fit) + "\n")
-    file.write("\n")
+autoencoder.fit(x_train, x_train, epochs=epochs, callbacks=[tensorboard_callback], validation_data=(x_test, x_test))
+autoencoder.save("Filtered_GNPSv2")
